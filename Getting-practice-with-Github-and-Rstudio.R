62b8595abc5540b6f05264cb08aaae4d13fef46b
mdm10-code <mdm10@illinois.edu>
2022-12-23 19:14:39 -0600

    This is my first commit for Getting Practice with Github and Rstudio Script

---
+++ Getting-practice-with-Github-and-Rstudio.R
@@ -0,0 +1,15 @@
+#####################################################################
+############ Practice for Github For Data Scientists ################
+#####################################################################
+
+#The purpose of this script is to gain practice with connecting 
+#RStudio with Github. 
+
+#First things first, let's load a required library
+library(lintr)
+
+#I am going to print a simple line of code.
+print("My name is Matthew Murphy, and I am improving myself through learning new things")
+
+#Before I commit, I am going to lint my script for best practice
+lint("C:/Users/mdm10/Documents/Personal-Data-Science-Projects/Github-for-data-scientists-practice/myrepo/Getting-practice-with-Github-and-Rstudio.R")
